5b9eb8475ffe3c5d20c2824d1f5d534fb1e7807d
Chris White <cwhite@sproutbox.com>
2011-12-16 15:27:09 -0500

    Songkick added some additional fields to venue

--- lib/songkickr/venue.rb
+++ lib/songkickr/venue.rb
@@ -1,12 +1,19 @@
 module Songkickr
   class Venue
-    attr_accessor :display_name, :id, :lat, :lng
+    attr_accessor :display_name, :id, :lat, :lng, :zip, :uri, :street, :website, :phone, :capacity, :description
     
     def initialize(venue_hash)
       @display_name = venue_hash["displayName"]
       @id           = venue_hash["id"]
       @lat          = venue_hash["lat"]
       @lng          = venue_hash["lng"]
+      @lng          = venue_hash["zip"]
+      @lng          = venue_hash["uri"]
+      @lng          = venue_hash["street"]
+      @lng          = venue_hash["website"]
+      @lng          = venue_hash["phone"]
+      @lng          = venue_hash["capacity"]
+      @lng          = venue_hash["description"]
     end
   end
 end
